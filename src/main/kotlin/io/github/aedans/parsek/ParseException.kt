0c44b669ca013b7a9beb531bcdf303f9c3a8ad37
Aedan <aedan.smith@gmail.com>
2017-11-13 23:09:46 -0800

    Remove ParseException.failure

--- src/main/kotlin/io/github/aedans/parsek/ParseException.kt
+++ src/main/kotlin/io/github/aedans/parsek/ParseException.kt
@@ -3,4 +3,4 @@ package io.github.aedans.parsek
 /**
  * Exception thrown on an non-propagateable parse error.
  */
-class ParseException(private val failure: ParseResult.Failure) : Exception("Parse exception: ${failure.err}")
+class ParseException(failure: ParseResult.Failure) : Exception("Parse exception: ${failure.err}")
