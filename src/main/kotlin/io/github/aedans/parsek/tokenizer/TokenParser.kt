94cd0f4499ba28217842472d1b8296380f6ad243
Aedan <aedan.smith@gmail.com>
2017-11-14 15:21:43 -0800

    Add tokenParser with ignore

--- src/main/kotlin/io/github/aedans/parsek/tokenizer/TokenParser.kt
+++ src/main/kotlin/io/github/aedans/parsek/tokenizer/TokenParser.kt
@@ -2,6 +2,9 @@ package io.github.aedans.parsek.tokenizer
 
 import io.github.aedans.parsek.Parser
 import io.github.aedans.parsek.conditionParser
+import io.github.aedans.parsek.dsl.or
+import io.github.aedans.parsek.dsl.skip
+import io.github.aedans.parsek.dsl.then
 
 /**
  * Creates a parser that parses tokens of type T.
@@ -9,5 +12,19 @@ import io.github.aedans.parsek.conditionParser
  * @param type The type of the token.
  */
 fun <A> tokenParser(type: A) = conditionParser({ it: Token<A> -> it.type == type }, { it })
+
+/**
+ * Creates a parser that parses tokens of type T.
+ * When the parser encounters an unexpected token, if it is in the ignored list, it skips that token.
+ *
+ * @see tokenParser
+ *
+ * @param type   The type of the token.
+ * @param ignore The list of token types to ignore.
+ */
+fun <A> tokenParser(type: A, ignore: List<A>) =
+        ignore.fold(tokenParser(type)) { parser, it ->
+            parser or (skip(tokenParser(it)) then parser)
+        }
 
 typealias TokenParser<A> = Parser<Token<A>, Token<A>>
